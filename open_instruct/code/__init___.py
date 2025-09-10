212c089a09defdec364c48b8c560fd5e5db3bd6c
sunyiyou <sunyiyou@berkeley.edu>
2025-09-10 16:37:06 -0700

    ballsim

--- open_instruct/code/__init___.py
+++ open_instruct/code/__init___.py
@@ -1,4 +1,27 @@
 from .api import app
-from .code_utils import get_successful_tests_fast
+# from .code_utils import get_successful_tests_fast
 
-__all__ = ["app", "get_successful_tests_fast"]
+from .ballsim_utils import (
+    check_distance,
+    decode_tests,
+    get_successful_tests_fast,
+    get_successful_tests_stdio,
+    run_tests_against_program_helper_2,
+    run_individual_test_helper,
+    should_execute,
+    reliability_guard,
+    partial_undo_reliability_guard
+)
+
+__version__ = "1.0.0"
+__all__ = [
+    'check_distance',
+    'decode_tests',
+    'get_successful_tests_fast',
+    'get_successful_tests_stdio',
+    'run_tests_against_program_helper_2',
+    'run_individual_test_helper',
+    'should_execute',
+    'reliability_guard',
+    'partial_undo_reliability_guard'
+] 
